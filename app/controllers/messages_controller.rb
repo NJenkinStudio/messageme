11999f5b7abbbddb075b36558297569b08d0f1f3
Koshokuar <koshokuar@rocketmail.com>
2021-03-15 17:13:04 -0400

    add stock tracking and table functionality

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -4,8 +4,10 @@ class MessagesController < ApplicationController
   def create
     @message = current_user.messages.build(message_params)
     if @message.save
-      ActionCable.server.broadcast "chatroom_channel", 
-                                    mod_message: message_render(@message)
+      ActionCable.server.broadcast "chatroom_channel",
+        respond_to do |format|
+          format.js { render partial: 'messages/message' }
+        end
     end
   end
 
@@ -13,10 +15,6 @@ class MessagesController < ApplicationController
   
   def message_params
     params.require(:message).permit(:body)
-  end
-
-  def message_render(message)
-    render partial: 'message', locals: {message: message}
   end
 
 end
